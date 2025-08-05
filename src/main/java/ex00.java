ee8bd8402a7dbc3551d6ef76108faa87f071a7cf
vtlschaf <vtlschaf@hotmail.com>
2025-08-05 11:45:19 +0300

    ex00

--- src/main/java/ex00.java
+++ src/main/java/ex00.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class ex00 {
+    public static void main(String[] args) {
+        int number = 479599;
+        int summ = 0;
+    while (number != 0) {
+        summ += (number % 10);
+        number /= 10;
+    }
+        System.out.println(summ);
+    }
 }
